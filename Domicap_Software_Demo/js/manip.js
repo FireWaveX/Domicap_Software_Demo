768c843c5f4f83a1dca212abe56fc88cb388d221
Loic BALLEYDIER <balleydl@iut-acy.local>
2017-03-15 19:05:58 +0100

    app content

--- Domicap_Software_Demo/js/manip.js
+++ Domicap_Software_Demo/js/manip.js
@@ -1,9 +1,42 @@
 $(document).ready(function(){
 
+	// $('.main-wrapper').toggleClass('light-on');
+	// $('body').toggleClass('light-on');
+	// $('header h1').toggleClass('light-on');
+
 	$('#light-app').click(function(){
-		$('.main-wrapper').toggleClass('light-on');
-		$('body').toggleClass('light-on');
-		$('header h1').toggleClass('light-on');
+
+		$('#light-content').toggle('slide', {direction: 'left'}, 250);
+	});
+
+	$('.screen').click(function() {
+
+		if(!clickOnApp($('#light-app')) 
+			&& !$('.content').is(':hover')){
+
+			$('#light-content').toggle('slide', {direction: 'left'}, 250);
+
+		}	
+
 	});
 
 });
+
+
+function clickOnApp($app = null) {
+	
+	var hover = false;
+
+	if($app == null)
+		$app = $('.app');
+
+	$app.each(function(){
+
+		if($(this).is(':hover'))
+			hover = true;
+
+	});
+
+	return hover;
+
+}
