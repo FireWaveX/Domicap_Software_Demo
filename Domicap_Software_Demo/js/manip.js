8a3b1be3b1287ea54c7a94bcb9f7b3d8a715460e
Loïc Balleydier <Loc Balleydier>
2017-03-25 18:08:42 +0100

    general information and heating freeze effect$

--- Domicap_Software_Demo/js/manip.js
+++ Domicap_Software_Demo/js/manip.js
@@ -22,7 +22,59 @@ $(document).ready(function(){
 		toogleLight(this.id);
 	});
 
+	$('.heat-ctrl').click(function(){
+		changeHeat(this);
+	});
+
+	displayDate();
+
 });
+
+function changeHeat(button){
+
+	var action = $(button).text();
+	var heat = Number($('.heat').text());
+	if(action == '-')
+		heat -= 0.5;
+	else
+		heat += 0.5;
+	$('.heat').text(heat);
+	changeHeatScreen(heat);
+
+}
+
+function changeHeatScreen(heat){
+
+	if(heat < 0){
+		$('.freeze-bg').css({opacity: 0.4});
+	}
+	else if(heat <= 15){
+		var x = ((15-heat)*0.4)/15;
+		$('.freeze-bg').css({opacity: x});
+	}
+	else if(heat >= 25){
+
+	}
+	else if(heat > 40){
+
+	}
+	else{
+		$('.freeze-bg').css({opacity: 0});
+	}
+
+}
+
+function displayDate(){
+
+	var date 	= new Date(Date.now());
+	var days	= ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'];
+	var months  = ['Janvier','Février','Mars','Avril','Mai','Juin','Juillet','Aout','Septembre','Octobre','Novembre','Décembre'];
+	var day 	= date.getDay();
+	var fullday = date.getDate();
+	var month 	= date.getMonth();
+	$('.date').html(days[day] + ' ' + fullday + "<br />" + months[month]);
+
+}
 
 
 function toggleApp(app){
